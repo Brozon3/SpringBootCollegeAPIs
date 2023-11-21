9095c2bceee7fd32439c9b71b2dbc36ce900f629
Pat Broders <patrick,broders73@ed.cna.nl.ca>
2023-11-21 13:56:39 -0330

    initial commit

---
+++ src/main/java/classes/Course.java
@@ -0,0 +1,48 @@
+package classes;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int courseID;
+    private String courseName;
+    private String courseNumber;
+    private int capacity;
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
---
+++ src/test/java/com/example/springcollegeapis/SpringCollegeApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcollegeapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCollegeApIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
